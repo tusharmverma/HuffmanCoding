f35c304fb73385d1bc8ea609eb3576bee6184dc2
Tushar Verma <tushar.m.verma@gmail.com>
2018-09-18 09:06:04 -0500

    Node Struct

--- huffmanCoding/main.cpp
+++ huffmanCoding/main.cpp
@@ -4,3 +4,32 @@
 #include <queue>
 #include <string>
 
+class HuffmanCodes
+{
+    struct Node
+    {
+        char data;
+        size_t freq;
+        Node* left;
+        Node* right;
+        Node(char data, size_t freq) : data(data),
+        freq(freq),
+        left(nullptr),
+        right(nullptr)
+        {}
+        ~Node()
+        {
+            delete left;
+            delete right;
+        }
+    };
+    
+    struct compare
+    {
+        bool operator()(Node* l, Node* r)
+        {
+            return (l->freq > r->freq);
+        }
+    };
+    
+};
