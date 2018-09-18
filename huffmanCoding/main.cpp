846f1e190fef72d440f04b09ac6ccdaefa7b37ca
Tushar Verma <tushar.m.verma@gmail.com>
2018-09-18 09:06:53 -0500

    Generate Huffman Code

--- huffmanCoding/main.cpp
+++ huffmanCoding/main.cpp
@@ -32,4 +32,63 @@ class HuffmanCodes
         }
     };
     
+    Node* top;
+    /*
+     * Source: http://www.geeksforgeeks.org/greedy-algorithms-set-3-huffman-coding/
+     */
+    void printCode(Node* root, std::string str)
+    {
+        if(root == nullptr)
+            return;
+        
+        if(root->data == '$')
+        {
+            printCode(root->left, str + "0");
+            printCode(root->right, str + "1");
+        }
+        
+        if(root->data != '$')
+        {
+            std::cout << root->data << " : " << str << "\n";
+            printCode(root->left, str + "0");
+            printCode(root->right, str + "1");
+        }
+    }
+    
+public:
+    HuffmanCodes() {};
+    ~HuffmanCodes()
+    {
+        delete top;
+    }
+    /*
+     * Source: http://www.geeksforgeeks.org/greedy-algorithms-set-3-huffman-coding/
+     */
+    void GenerateCode(std::vector<char>& data, std::vector<size_t>& freq, size_t size)
+    {
+        Node* left;
+        Node* right;
+        std::priority_queue<Node*, std::vector<Node*>, compare > minHeap;
+        
+        for(size_t i = 0; i < size; ++i)
+        {
+            minHeap.push(new Node(data[i], freq[i]));
+        }
+        
+        while(minHeap.size() != 1)
+        {
+            left = minHeap.top();
+            minHeap.pop();
+            
+            right = minHeap.top();
+            minHeap.pop();
+            
+            top = new Node('$', left->freq + right->freq);
+            top->left  = left;
+            top->right = right;
+            minHeap.push(top);
+        }
+        printCode(minHeap.top(), "");
+    }
+    
 };
